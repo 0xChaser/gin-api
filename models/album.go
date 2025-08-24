98c990736aaa7d583d64de5e8d88136a826cd9d5
chaser <florian.isak@icloud.com>
2025-08-24 19:05:16 +0200

    Add albums model

---
+++ models/album.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Album struct {
+	ID     uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
+	Title  string    `json:"title" gorm:"not null"`
+	Artist string    `json:"artist" gorm:"not null"`
+	Price  float64   `json:"price" gorm:"not null"`
+	gorm.Model
+}
+
+func (album *Album) BeforeCreate(tx *gorm.DB) error {
+	if album.ID == uuid.Nil {
+		album.ID = uuid.New()
+	}
+	return nil
+}
